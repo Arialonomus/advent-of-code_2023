83e8588da52beccb3b518aff1e2eb8eeb08e4110
Jacob Barber <barber.jacobervin@gmail.com>
2024-09-05 18:57:23 -0500

    Added name_to_digit() helper function to convert digit names to their corresponding integer

---
+++ day_01/utils_day_01.py
@@ -0,0 +1,25 @@
+def name_to_digit(str):
+    """Converts a string representing the name of a single digit
+    to the corresponding digit 1-9, returns zero on error
+    """
+    match str:
+        case "one":
+            return 1
+        case "two":
+            return 2
+        case "three":
+            return 3
+        case "four":
+            return 4
+        case "five":
+            return 5
+        case "six":
+            return 6
+        case "seven":
+            return 7
+        case "eight":
+            return 8
+        case "nine":
+            return 9
+        case _:
+            return 0
