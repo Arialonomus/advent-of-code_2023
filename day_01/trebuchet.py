0ade688c10100226061a14543c5bf481dbbfe0e6
Jacob Barber <barber.jacobervin@gmail.com>
2024-09-05 18:41:45 -0500

    Added code to parse input lines per Part 1 specs

--- day_01/trebuchet.py
+++ day_01/trebuchet.py
@@ -6,10 +6,29 @@ Solution by Jacob Barber
 """
 
 import argparse
+import re
 
 # Parse arguments
 parser = argparse.ArgumentParser(description="A solution to Advent of Code '23 puzzle 'Trebuchet!?' Part 1.")
 parser.add_argument('input_file',
                     type=argparse.FileType('r'),
                     help="Input file path")
-args = parser.parse_args
+args = parser.parse_args()
+
+# Read each line and parse two digit numbers from them
+sum = 0
+with args.input_file as file:
+    for line in file:
+        # Parse digits in line into list
+        digits = re.findall(r'\d', line)
+
+        # Select first and last digits (same if only 1 digit present)
+        first = digits[0]
+        last = digits[len(digits) - 1]
+
+        # Combine digits into calibration value and add to sum
+        digit_str = first + last
+        sum += int(digit_str)
+
+# Print the sum of the digits in each line
+print(sum)
