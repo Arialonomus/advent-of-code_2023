67a10171c8f19f6c30be03218d9a5539914316bd
Jacob Barber <barber.jacobervin@gmail.com>
2024-09-05 19:21:55 -0500

    Updated name_to_digit() to return a character instead of an integer

--- day_01/utils_day_01.py
+++ day_01/utils_day_01.py
@@ -1,25 +1,25 @@
 def name_to_digit(str):
     """Converts a string representing the name of a single digit
-    to the corresponding digit 1-9, returns zero on error
+    to the corresponding character 1-9, returns '0' on error
     """
     match str:
         case "one":
-            return 1
+            return '1'
         case "two":
-            return 2
+            return '2'
         case "three":
-            return 3
+            return '3'
         case "four":
-            return 4
+            return '4'
         case "five":
-            return 5
+            return '5'
         case "six":
-            return 6
+            return '6'
         case "seven":
-            return 7
+            return '7'
         case "eight":
-            return 8
+            return '8'
         case "nine":
-            return 9
+            return '9'
         case _:
-            return 0
+            return '0'
