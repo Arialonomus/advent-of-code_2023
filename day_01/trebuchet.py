eb1393b70deb5a78310212d9a0f2c6eaac3ae111
Jacob Barber <barber.jacobervin@gmail.com>
2024-09-05 19:22:22 -0500

    Updated script to handle cases where the digit name is listed, per Part 2 specs

--- day_01/trebuchet.py
+++ day_01/trebuchet.py
@@ -1,16 +1,17 @@
 """
 Advent of Code 2023
-Day 1 - Trebuchet!? - Puzzle 1
+Day 1 - Trebuchet!?
 
 Solution by Jacob Barber
 """
 
 import argparse
 import re
+from utils_day_01 import name_to_digit
 
 # Parse arguments
 parser = argparse.ArgumentParser(
-    description="A solution to Advent of Code '23 puzzle 'Trebuchet!?'")
+    description="A solution to Advent of Code 2023 puzzle 'Trebuchet!?'")
 parser.add_argument('input_file',
                     type=argparse.FileType('r'),
                     help="Input file path")
@@ -21,14 +22,20 @@ sum = 0
 with args.input_file as file:
     for line in file:
         # Parse digits in line into list
-        digits = re.findall(r'\d', line)
+        pattern = r'\d|one|two|three|four|five|six|seven|eight|nine'
+        digits = re.findall(pattern, line)
 
         # Select first and last digits (same if only 1 digit present)
         first = digits[0]
+        if len(first) > 1:
+            first = name_to_digit(first)
         last = digits[len(digits) - 1]
+        if len(last) > 1:
+            last = name_to_digit(last)
 
         # Combine digits into calibration value and add to sum
         digit_str = first + last
+        print(str(digits) + ' ' + digit_str)
         sum += int(digit_str)
 
 # Print the sum of the digits in each line
