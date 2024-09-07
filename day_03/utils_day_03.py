f89b41c23ca0f3ae65399e9fb7f2f9b18529ad58
Jacob Barber <barber.jacobervin@gmail.com>
2024-09-06 22:45:45 -0500

    Fixed bug in check_cell() where '.' characters were not being checked

--- day_03/utils_day_03.py
+++ day_03/utils_day_03.py
@@ -3,7 +3,7 @@ def check_cell(cell_contents):
     Checks the passed in contents of a cell to determine if it is a symbol.
     Returns true if so, otherwise returns false.
     """
-    if cell_contents.isdigit() or check_cell == '.':
+    if cell_contents.isdigit() or cell_contents == '.':
         return False
     else:
         return True
