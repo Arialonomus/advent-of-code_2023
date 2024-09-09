e094a7b8b1453f8f0ae422c4fbd0f96be2cc7be0
Jacob Barber <barber.jacobervin@gmail.com>
2024-09-09 16:40:42 -0500

    Implemented solution to puzzle part two by building a list tree

--- day_04/scratchcards-part2.py
+++ day_04/scratchcards-part2.py
@@ -7,6 +7,16 @@ Solution by Jacob Barber
 
 from aoc_utils import get_args
 
+def count_elements(input_list):
+    """Recursively counts the elements in a list, including nested list elements"""
+    total = 0
+    for element in input_list:
+        if isinstance(element, list):
+            total += count_elements(element)
+        else:
+            total += 1
+    return total
+
 # Parse arguments
 args = get_args(4, "Scratchcards")
 
@@ -30,16 +40,22 @@ with args.input_file as file:
             "mine": my_nums
         }
 
-    """# Check each winning number against the owned numbers for a match
-    sum_cards = 0
-    num_matches = 0
-    points = 0
-    for num in winning_nums:
-        if num in my_nums:
-            points = 2**num_matches
-            num_matches += 1
-    sum_points += points
+    # Calculate the number of matches for each scratchcard
+    num_original = len(scratchcards)
+    matches = [0] * num_original
+    for card in scratchcards:
+        for num in scratchcards[card]["winning"]:
+            if num in scratchcards[card]["mine"]:
+                matches[card - 1] += 1
 
-    # Display points for pile of scratchcards
-    print(sum_points)
-    """
+    # Construct a list tree for the cards won
+    cards_won = [0] * num_original
+    for i in range(num_original - 1, -1, -1):
+        cards_won[i] = [i+1]
+        if matches[i] > 0:
+            for j in range(i + 1, i + matches[i] + 1):
+                cards_won[i].append(cards_won[j])
+
+    # Count and print the number of cards won
+    num_cards = count_elements(cards_won)
+    print(num_cards)
