7ce812262962cbc906546efd6c2f000e079b7fe1
Jacob Barber <barber.jacobervin@gmail.com>
2024-09-10 08:33:11 -0500

    Implemented code to parse mappings into almanac data structure

---
+++ day_05/fertilizer-part1.py
@@ -0,0 +1,35 @@
+"""
+Advent of Code 2023
+Day 5 - If You Give A Seed A Fertilizer (Part 1)
+
+Solution by Jacob Barber
+"""
+
+from aoc_utils import get_args
+
+# Parse arguments
+args = get_args(5, "If You Give A Seed A Fertilizer")
+
+with args.input_file as file:
+    # Parse the list of initial seed numbers
+    initial_seeds = list(map(int, file.readline().rstrip().strip("seeds: ").split()))
+
+    # Construct the almanac by parsing the remainder of the file
+    almanac = []
+    file.readline()         # Consume newline for first mapping
+    line = file.readline()  # Read in first header
+    # Read in lines until EOF
+    while line != '':
+        mapping_title = line.rstrip(" map:\n")
+        mapping = []
+        line = file.readline()  # Read first mapping line
+        # Parse each mapping line into tuple of range start, range end, and function map
+        while line != '\n':
+            destination, source, range_len = list(map(int, line.rstrip().split()))
+            mapping.append((source, source + range_len, destination - source))
+            line = file.readline()
+        almanac.append({
+            'title': mapping_title,
+            'mapping': sorted(mapping)
+        })
+        line = file.readline() # Read next header, or EOF
