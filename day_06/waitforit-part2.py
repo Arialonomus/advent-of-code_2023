da2e541c8cc582c9d9b9cc4842750b32562df45d
Jacob Barber <barber.jacobervin@gmail.com>
2024-09-11 13:21:56 -0500

    Updated documentation

--- day_06/waitforit-part2.py
+++ day_06/waitforit-part2.py
@@ -18,11 +18,14 @@ with args.input_file as file:
     distance = int(file.readline().replace(" ", "").strip("Distance:").strip())
 
     # Solutions for the longest and shortest times the button can be held
-    # is given by v^2 -tv + d = 0 where v = time held, t = race time,
+    # is given by v^2 - tv + d = 0 where v = time held, t = race time,
     # and d = the minimum distance needed to win (distance + 1)
     coefficients = [1, time * -1, distance + 1]
     solutions = np.roots(coefficients)
 
     # Calculate the number of ways to win the race
+    # solutions[0] represents the max number of ms the button can be held, must round down
+    # solutions[1] represents the min number of ms, must round up
+    # Add 1 to make the range inclusive
     num_permutations = math.floor(solutions[0]) - math.ceil(solutions[1]) + 1
     print(num_permutations)
