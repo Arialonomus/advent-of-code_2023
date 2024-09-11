02b4cdec9d8d26a96ef9a580c4f6a8be27d8022e
Jacob Barber <barber.jacobervin@gmail.com>
2024-09-11 13:19:07 -0500

    Modified solution to parse and handle inputs as one value each, per part 2 specs

---
+++ day_06/waitforit-part2.py
@@ -0,0 +1,28 @@
+"""
+Advent of Code 2023
+Day 6 - Wait For It (Part 2)
+
+Solution by Jacob Barber
+"""
+
+from aoc_utils import get_args
+import math
+import numpy as np
+
+# Parse arguments
+args = get_args(6, "Wait For It")
+
+with args.input_file as file:
+    # Parse input data
+    time = int(file.readline().replace(" ", "").strip("Time:").strip())
+    distance = int(file.readline().replace(" ", "").strip("Distance:").strip())
+
+    # Solutions for the longest and shortest times the button can be held
+    # is given by v^2 -tv + d = 0 where v = time held, t = race time,
+    # and d = the minimum distance needed to win (distance + 1)
+    coefficients = [1, time * -1, distance + 1]
+    solutions = np.roots(coefficients)
+
+    # Calculate the number of ways to win the race
+    num_permutations = math.floor(solutions[0]) - math.ceil(solutions[1]) + 1
+    print(num_permutations)
