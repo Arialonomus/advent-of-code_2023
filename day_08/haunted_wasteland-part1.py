9d39a61fa8208744f8886a665b26e32ad1725dd2
Jacob Barber <barber.jacobervin@gmail.com>
2024-09-12 08:19:44 -0500

    Implemented map traversal algorithm to solve part 1

--- day_08/haunted_wasteland-part1.py
+++ day_08/haunted_wasteland-part1.py
@@ -22,3 +22,21 @@ with args.input_file as file:
     for line in file:
         node, left, right = line.translate(translation_table).split()
         desert_map[node] = (left, right)
+
+# Traverse the map following the instructions, summing the number of steps
+num_steps = 0
+current_node = START_NODE
+i = 0
+while current_node != END_NODE:
+    instruction = instructions[i]
+    if instruction == 'L':
+        current_node = desert_map[current_node][0]
+    else:
+        current_node = desert_map[current_node][1]
+    num_steps += 1
+    i += 1
+    if i == len(instructions):
+        i = 0
+
+# Display total steps required
+print(num_steps)
