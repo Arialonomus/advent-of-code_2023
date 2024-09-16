490af49058e992ddf2ef594ebf3c5677ce9b6e1b
Jacob Barber <barber.jacobervin@gmail.com>
2024-09-16 08:35:40 -0500

    Replaced brute-force search with range mapping algorithm to more efficiently handle large seed ranges. Solved part 2.

--- day_05/fertilizer-part2.py
+++ day_05/fertilizer-part2.py
@@ -18,6 +18,7 @@ with args.input_file as file:
     while j < len(initial_seeds) - 1:
         seed_ranges.append((initial_seeds[j], initial_seeds[j] + initial_seeds[j + 1]))
         j += 2
+    seed_ranges.sort()
 
     # Construct the almanac by parsing the remainder of the file
     almanac = []
@@ -27,35 +28,59 @@ with args.input_file as file:
         mapping_title = file.readline().rstrip(" map:\n")
         mapping = []
         line = file.readline()  # Read first mapping line
-        # Parse each mapping line into tuple of range start, range end, and function map
         while line != '\n' and line != '':
+            # Parse line into tuple of range start, range end, and transformation
             destination, source, range_len = list(map(int, line.rstrip().split()))
             mapping.append((source, source + range_len, destination - source))
+
+            # Read in next line
             line = file.readline()
-        almanac.append({
-            'title': mapping_title,
-            'mapping': sorted(mapping)
-        })
+        almanac.append(sorted(mapping))
 
-    # Calculate the locations for each seed
-    locations = []
-    for i in range(0, len(seed_ranges)):
-        seed_range = seed_ranges[i]
-        locations.append(seed_range[0])
-        for seed in range(seed_range[0], seed_range[1]):
-            for entry in almanac:
-                mapping_found = False
-                j = 0
-                while j < len(entry['mapping']) and not mapping_found:
-                    mapping = entry['mapping'][j]
-                    if mapping[1] > seed >= mapping[0]:
-                        seed += mapping[2]
-                        mapping_found = True
+# Map the input ranges to their final locations
+inputs = seed_ranges
+outputs = []
+for mapping_set in almanac:
+    while inputs:
+        input_start, input_end = inputs.pop(0)
+        match_found = False
+        i = 0
+        while i < len(mapping_set) and not match_found:
+            map_range_start, map_range_end, transformation = mapping_set[i]
+            if input_start < map_range_end:
+                # Input range within ending boundary of map range
+                if input_start < map_range_start:
+                    # Input range start falls before or between mappings
+                    output_start = input_start
+                    if input_end < map_range_start:
+                        # Input range is outside any mapping
+                        output_end = input_end
                     else:
-                        j += 1
-            if seed < locations[i]:
-                locations[i] = seed
+                        # Subdivide input range with new start boundary at map start
+                        output_end = map_range_start
+                        inputs.insert(0, (map_range_start, input_end))
+                else:
+                    # Input range within starting boundary of map range
+                    output_start = input_start + transformation
+                    if input_end < map_range_end:
+                        # Input range fully contained in map range
+                        output_end = input_end + transformation
+                    else:
+                        # Subdivide input range with new start boundary at map end
+                        output_end = map_range_end + transformation
+                        inputs.insert(0, (map_range_end, input_end))
+                outputs.append((output_start, output_end))
+                match_found = True
+            i += 1
+        if not match_found:
+            # Input range falls beyond outer bounds of all mappings in this step
+            outputs.append((input_start, input_end))
 
-    # Determine and print the lowest location
-    lowest_location = min(locations)
-    print(lowest_location)
+    # Swap buffers for next iteration
+    inputs = sorted(outputs)
+    outputs = []
+
+# Final locations will be stored in inputs
+locations = inputs
+lowest_location_number = locations[0][0]
+print(lowest_location_number)
