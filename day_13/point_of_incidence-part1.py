bcaf7ab3b07c59bc2d6cc12568d0608f08dae7a4
Jacob Barber <barber.jacobervin@gmail.com>
2024-09-17 09:06:22 -0500

    Implemented solution for counting perfect reflections across rows and columns. Solved part 1.

--- day_13/point_of_incidence-part1.py
+++ day_13/point_of_incidence-part1.py
@@ -28,3 +28,50 @@ with args.input_file as file:
             line = file.readline()
         patterns.append((rows, cols))
         line = file.readline()
+
+def get_total_reflections(pattern):
+    """
+    Returns the number of horizontal and vertical reflections for
+    a passed in pattern, with the horizontal reflections scaled
+    by a pre-set Horizontal Reflection Factor.
+    """
+
+    def count_reflections(tile_list):
+        """
+        Returns the number of reflected rows/columns across a line of
+        prefect reflection found within the passed in list of rows/columns.
+        If no line of perfect reflection is found, returns zero.
+        """
+
+        list_len = len(tile_list)
+        for i in range(list_len - 1):
+            # Check if adjacent lines are reflected
+            list_a = tile_list[i]
+            list_b = tile_list[i + 1]
+            if list_a == list_b:
+                # Line of reflection found
+                a = i - 1
+                b = i + 2
+                perfect_reflection = True
+                while a >= 0 and b < list_len and perfect_reflection:
+                    if tile_list[a] == tile_list[b]:
+                        a -= 1
+                        b += 1
+                    else:
+                        perfect_reflection = False
+                if perfect_reflection:
+                    return i + 1
+
+        return 0
+
+    row_list, column_list = pattern
+    num_reflected_rows = count_reflections(row_list)
+    num_reflected_cols = count_reflections(column_list)
+
+    return (HORIZONTAL_REFLECTION_FACTOR * num_reflected_rows) + num_reflected_cols
+
+# Calculate and display the sum of total reflections across all patterns
+sum_reflection_counts = 0
+for pattern in patterns:
+    sum_reflection_counts += get_total_reflections(pattern)
+print(sum_reflection_counts)
