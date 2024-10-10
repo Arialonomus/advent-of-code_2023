089f2cfc1404e6b77cd5515e4cf473b62c3f81ea
Jacob Barber <barber.jacobervin@gmail.com>
2024-10-10 16:04:41 -0500

    Refactored solution to calculate number of boundary points during input read

--- day_18/lavaduct_lagoon-part2.py
+++ day_18/lavaduct_lagoon-part2.py
@@ -12,12 +12,19 @@ args = get_args(18, "Lavaduct Lagoon (Part 2)")
 
 # Read in input and parse the instructions into vertices
 with args.input_file as file:
+    num_boundary_points = 0 # Number of boundary points will always be the sum of the distances
     vertices = [(0,0)]
     x, y = 0, 0
     for line in file:
+        # Parse the hex code into direction and distance
         hex_string = line.strip()[-7:-1]
         direction = int(hex_string[-1])
         distance = int(hex_string[:-1], 16)
+
+        # Accumulate boundary points
+        num_boundary_points += distance
+
+        # Calculate the next vertex in the shape
         match direction:
             # Right
             case 0:
@@ -33,23 +40,13 @@ with args.input_file as file:
                 y += distance
         vertices.append((x,y))
 
-# Calculate the boundary points and area sum for the shape
-num_boundary_points = 0
+# Apply the shoelace formula to calculate the area of the shape
 num_vertices = len(vertices)
 area_sum = 0
 for i in range(num_vertices - 1):
-    # For each pair of vertices, we will count the points along the line
-    vertex1, vertex2 = vertices[i], vertices[i + 1]
-    x1, y1 = vertex1
-    x2, y2 = vertex2
-
-    # Since all lines are horizontal or vertical, one result will always be zero
-    num_boundary_points += max(abs(x2 - x1), abs(y2 - y1))
-
-    # Calculate the area sum for use in the shoelace formula
+    x1, y1 = vertices[i]
+    x2, y2 = vertices[i + 1]
     area_sum += ((x1 * y2) - (x2 * y1))
-
-# Apply the shoelace formula to get the integer area of the shape
 area = abs(area_sum) // 2
 
 # Calculate the total number of points enclosed in the shape using Pick's Theorem
